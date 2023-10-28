c0d9ae2aea54564eb03a4a691decb86a5ca8ab8b
RyuRi <100249997+RyuRi996@users.noreply.github.com>
2023-10-29 02:23:16 +0700

    change style

--- app/src/main/java/com/example/imagegallery/MainActivity.java
+++ app/src/main/java/com/example/imagegallery/MainActivity.java
@@ -21,11 +21,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         buttonImage();
+
     }
 
     private void buttonImage() {
-
         ArrayList<Integer> imageList = getImageDrawable();
         title = findViewById(R.id.titleImage);
         imageView = findViewById(R.id.imageShow);
